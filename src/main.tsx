b3abeab9f9c13c2dd2586e6133dd8ed22a1ccd87
Enrique Dick <iscjedb@gmail.com>
2022-08-14 12:21:09 -0500

    Suspense added

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes";
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "./index.css";
+import Loading from "./components/Loading";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppRoutes />
-    </Provider>
+    <Suspense fallback={<Loading />}>
+      <Provider store={store}>
+        <AppRoutes />
+      </Provider>
+    </Suspense>
   </React.StrictMode>
 );

6418eee41c148e67bfb5596ec4751c740ab2f090
Enrique Dick <iscjedb@gmail.com>
2022-08-14 12:53:48 -0500

    NotFound view improvements

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 
@@ -6,9 +5,10 @@ export default function NotFound() {
   return (
     <>
       <Helmet>
-        <title>Kanban App - Login</title>
+        <title>Kanban App - Not Found</title>
       </Helmet>
       <div>
+        <h1>404! Page Not Found</h1>
         <h2>Nothing to see here!</h2>
         <p>
           <Link to="/">Go to the home page</Link>

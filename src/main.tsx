f97da24df710a32b194bf72647ea8e126aceab56
Enrique Dick <iscjedb@gmail.com>
2022-08-14 14:07:41 -0500

    Added Lazy loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,15 @@
-import React, { Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes";
 import { Provider } from "react-redux";
 import store from "./store";
 
 import "./index.css";
-import Loading from "./components/Loading";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Suspense fallback={<Loading />}>
-      <Provider store={store}>
-        <AppRoutes />
-      </Provider>
-    </Suspense>
+    <Provider store={store}>
+      <AppRoutes />
+    </Provider>
   </React.StrictMode>
 );
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,18 +1,18 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { HashRouter, Routes, Route } from "react-router-dom";
 
 // Middlewares
 import RequireAuth from "../middlewares/RequireAuth";
 
 // Pages
-import Backlog from "./../pages/Backlog";
-// const Backlog = lazy(() => import("./../pages/Backlog"));
-import Home from "./../pages/Home";
+const Backlog = lazy(() => import("./../pages/Backlog"));
+const Home = lazy(() => import("./../pages/Home"));
 import Login from "./../pages/Login";
 import NotFound from "./../pages/NotFound";
 
 import App from "./../App";
-import SignIn from "../pages/SignIn";
+const SignIn = lazy(() => import("./../pages/SignIn"));
+import Loading from "../components/Loading";
 
 export const ROUTES = {
   PUBLIC: {
@@ -28,18 +28,20 @@ export const ROUTES = {
 export default function AppRoutes() {
   return (
     <HashRouter>
-      <Routes>
-        <Route path={ROUTES.PRIVATE.HOME} element={<App />}>
-          <Route element={<RequireAuth />}>
-            <Route index element={<Home />} />
-            <Route path={ROUTES.PRIVATE.BACKLOG} element={<Backlog />} />
-          </Route>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path={ROUTES.PRIVATE.HOME} element={<App />}>
+            <Route element={<RequireAuth />}>
+              <Route index element={<Home />} />
+              <Route path={ROUTES.PRIVATE.BACKLOG} element={<Backlog />} />
+            </Route>
 
-          <Route path={ROUTES.PUBLIC.LOGIN} element={<Login />} />
-          <Route path={ROUTES.PUBLIC.SIGNIN} element={<SignIn />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+            <Route path={ROUTES.PUBLIC.LOGIN} element={<Login />} />
+            <Route path={ROUTES.PUBLIC.SIGNIN} element={<SignIn />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </HashRouter>
   );
 }

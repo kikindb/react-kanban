54880c92857de8a880598047ae40afa929f5767e
Enrique Dick <iscjedb@gmail.com>
2022-08-14 12:22:29 -0500

    Removed Tasks redux logic

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth";
-import tasksReducer from "./tasks";
 import alertReducer from "./alert";
 
 const store = configureStore({
-  reducer: { tasks: tasksReducer, auth: authReducer, alert: alertReducer },
+  reducer: { auth: authReducer, alert: alertReducer },
 });
 
 export default store;
